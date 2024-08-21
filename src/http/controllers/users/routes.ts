dfcc52b20a2a6091fdf19c5fb7df4593ec230d32
Diego Ferreira <diegoferreira1964@gmail.com>
2024-08-21 19:43:00 -0300

    chore: gym creation controller

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,10 +1,11 @@
 import { FastifyInstance } from 'fastify'
-import { authenticate } from '@/http/controllers/authenticate'
-import { register } from './controllers/register'
-import { profile } from '@/http/controllers/profile'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
+import { authenticate } from './authenticate'
+import { register } from './register'
+import { profile } from './profile'
 
-export async function appRoutes(app: FastifyInstance) {
+// export async function appRoutes(app: FastifyInstance) {
+export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
