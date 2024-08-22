840871b4717760ce18d2f0e11917d8812534f165
Diego Ferreira <diegoferreira1964@gmail.com>
2024-08-22 20:53:02 -0300

    chore: implementation of refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,13 +1,17 @@
 import { FastifyInstance } from 'fastify'
-import { verifyJwt } from '@/http/middlewares/verify-jwt'
-import { authenticate } from './authenticate'
-import { register } from './register'
-import { profile } from './profile'
 
-// export async function appRoutes(app: FastifyInstance) {
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
+
+import { authenticate } from './authenticate'
+import { profile } from './profile'
+import { register } from './register'
+import { refresh } from './refresh'
+
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /** Authenticated */
   app.get('/me', { onRequest: [verifyJwt] }, profile)
